3055672226c7ed88497d7ed2e31d9426097b8e4d
sheharyarghouri <mshahryarkghouri@gmail.com>
2024-09-25 01:44:37 +0500

    completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 var userName = prompt("Insert you name :");
-var rollNo = prompt("Insert you Roll No :");
+var rollNo = +prompt("Insert you Roll No :");
 var english = +prompt("Insert you English Marks :");
 var urdu = +prompt("Insert you Urdu Marks :");
 var physics = +prompt("Insert you Physics Marks :");
@@ -12,9 +12,26 @@ var mathematics = +prompt("Insert you Mathematics Marks :");
 var total = english + urdu + physics + chemistry + mathematics;
 var percentage = (total / 500) * 100;
 
+var total;
+if (total > 500) {
+    total = "Invalid"
+}
+
+
+var percentage;
+if (percentage > 100) {
+    percentage = "Please fill correct numbers";
+}
+
+// if (english <= 100 && urdu <= 100 && physics <= 100 && chemistry <= 100 && mathematics <= 100) {
+//     else (
+
+//     )
+// }
+
 
 var grade;
-if (percentage >= 90 && percentage < 101) {
+if (percentage >= 90 && percentage <= 100) {
     grade = 'A+';
 }
 
@@ -23,23 +40,27 @@ else if (percentage >= 80 && percentage < 90) {
 }
 
 else if (percentage >= 70 && percentage < 80) {
-    grade = 'B';
+    grade = 'B+';
 }
 
 else if (percentage >= 60 && percentage < 70) {
-    grade = 'C';
+    grade = 'B';
 }
 
 else if (percentage >= 50 && percentage < 60) {
-    grade = 'D';
+    grade = 'c';
 }
 else if (percentage < 50) {
     grade = 'Fail';
 }
+// else if (percentage>100){
+//     alert("Please insert valid value")
+// }
 
 else {
     grade = ("please type correct marks");
 }
+
 
 
 alert("Your Name : " + userName + " \nTotal Marks : " + total + " \nPercentage : " + percentage + " \nGrade : " + grade);
